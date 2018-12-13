b54f214d61a7fa2572927f4748a33a17fd419b67
Rlopez18 <roberto.lopez16@upr.edu>
2018-12-13 03:54:04 -0400

    Fixed the arrows in the tutorial (Estaban media algarete) also added new edited pictures. The text in them can be edited to look bigger.

--- app/src/main/java/com/archivouni/guiauniversitaria/TutorialActivity.kt
+++ app/src/main/java/com/archivouni/guiauniversitaria/TutorialActivity.kt
@@ -52,56 +52,6 @@ class TutorialActivity : AppCompatActivity() {
             if (mCurrentScreen == 0)
                 it.visibility = View.GONE
         }
-//        if (mCurrentScreen == 1){
-//
-//            textTutorial01.visibility = View.VISIBLE
-//            textTutorial02.visibility = View.INVISIBLE
-//            textTutorial03.visibility = View.INVISIBLE
-//            textTutorial04.visibility = View.INVISIBLE
-//            textTutorial05.visibility = View.INVISIBLE
-//            textTutorial06.visibility = View.INVISIBLE
-//        }
-//        else if(mCurrentScreen == 2){
-//            textTutorial02.visibility = View.VISIBLE
-//            textTutorial01.visibility = View.INVISIBLE
-//            textTutorial03.visibility = View.INVISIBLE
-//            textTutorial04.visibility = View.INVISIBLE
-//            textTutorial05.visibility = View.INVISIBLE
-//            textTutorial06.visibility = View.INVISIBLE
-//        }
-//        else if(mCurrentScreen == 3){
-//            textTutorial03.visibility = View.VISIBLE
-//            textTutorial01.visibility = View.INVISIBLE
-//            textTutorial02.visibility = View.INVISIBLE
-//            textTutorial04.visibility = View.INVISIBLE
-//            textTutorial05.visibility = View.INVISIBLE
-//            textTutorial06.visibility = View.INVISIBLE
-//        }
-//        else if(mCurrentScreen == 4){
-//            textTutorial04.visibility = View.VISIBLE
-//            textTutorial01.visibility = View.INVISIBLE
-//            textTutorial02.visibility = View.INVISIBLE
-//            textTutorial03.visibility = View.INVISIBLE
-//            textTutorial05.visibility = View.INVISIBLE
-//            textTutorial06.visibility = View.INVISIBLE
-//        }
-//        else if(mCurrentScreen == 5){
-//            textTutorial05.visibility = View.VISIBLE
-//            textTutorial01.visibility = View.INVISIBLE
-//            textTutorial02.visibility = View.INVISIBLE
-//            textTutorial04.visibility = View.INVISIBLE
-//            textTutorial03.visibility = View.INVISIBLE
-//            textTutorial06.visibility = View.INVISIBLE
-//        }
-//        else{
-//            textTutorial06.visibility = View.VISIBLE
-//            textTutorial01.visibility = View.INVISIBLE
-//            textTutorial02.visibility = View.INVISIBLE
-//            textTutorial04.visibility = View.INVISIBLE
-//            textTutorial05.visibility = View.INVISIBLE
-//            textTutorial03.visibility = View.INVISIBLE
-//        }
-
         mQuitButton = findViewById(R.id.tutorial_quit_button)
         mQuitButton.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
